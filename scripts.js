1363656cebf5b1ebbe7e6496c6275aff7f2c2137
Nikolas Strohecker <sixniks2@gmail.com>
2024-12-14 14:03:05 -0700

    Add Keyboard Support and Backspace

--- scripts.js
+++ scripts.js
@@ -1,14 +1,4 @@
-//Can probably just do split() on the operators and take each piece for the variables
-
-//CURRENT PROBLEM:
-//Expresions with multiple operators do not split correctly 
-
-
-
-//TO DO:
-//ADD KEYBOARD SUPPORT
-//ADD BACKSPACE
-
+//Gonna be honest dont know if these variables are doing anything
 let num1=""
 let num2=""
 let operator="+"
@@ -19,7 +9,6 @@ let commaString=","
 const add = function(num1,num2,runoffARR) {
     let sum=Number(num1)+Number(num2)
     displayText.textContent=sum+" "+runoffARR
-    console.log("ADDITION")
 	return Number(sum)
 
 };
@@ -30,7 +19,6 @@ const subtract = function(num1,num2,runoffARR) {
    console.log(newNum1,newNum2,"newNum1,newNum2")
     let remainder=Number(newNum1)-Number(newNum2)
     displayText.textContent=remainder+" "+runoffARR
-    console.log("SUBTRACT")
 	return Number(remainder)
 };
 
@@ -38,14 +26,12 @@ const subtract = function(num1,num2,runoffARR) {
 const multiply = function(num1,num2,runoffARR) {
     let product=Number(num1)*Number(num2)
     displayText.textContent=product+" "+runoffARR
-    console.log(num1,num2,"MULTIPLY")
 	return Number(product)
 };
 
 const divide = function(num1,num2,runoffARR){
     let quotient=Number(num1)/Number(num2)
     displayText.textContent=quotient+" "+runoffARR
-    console.log("DIVIDE")
 	return Number(quotient)
 }
 
@@ -68,26 +54,48 @@ document.querySelectorAll('button').forEach((button) => {
     }
     else if(btn.classList.contains("equalbutton"))
     {
-        console.log("equals")
         operate()
+    }
+    else if(btn.classList.contains("backspace")){
+        displayText.innerText=displayText.textContent.replace(/.$/, '')
     }
         else displayText.innerText=displayText.innerText+buttonText
         
      
-   
-        
+      
+  }
+
+  
+    document.addEventListener('keydown', keybrdEvent);
+
+        function keybrdEvent(event){
+            let keypressed= event.key
+            console.log(keypressed)
+            if(keypressed==="/"){
+                displayText.innerText=displayText.innerText+" "+"÷"
+            }
+            else if((["+","-","*","/"].includes(keypressed))){
+                displayText.innerText=displayText.innerText+" "+keypressed
+            }
+           else if(keypressed==="Enter"){
+                operate()
+            }
+           else if(keypressed===!"+"||"-"||"*"||"/"||"1"||"2"||"3"||"4"||"5"||"6"||"7"||"8"||"9"||"0"||"Enter"
+           ){displayText.innerText=displayText.innerText+keypressed} 
+
+}
+
+
    //Split on each operator then check for sign
        function operate(){
  
-        if( displayText.textContent.includes("+") ||
+        if( displayText.textContent.includes("+")||
             displayText.textContent.includes("-")||
             displayText.textContent.includes("*")||
             displayText.textContent.includes("÷")){
             let newARR= displayText.textContent.split(" ")
             let expStr= newARR[0]+newARR[1]
             let runoffARR= newARR.slice(2).join(" ")
-            console.log(runoffARR,"runoffARR")
-            console.log(expStr,"expStr")
                 if(expStr.includes("+")){
                     expARR= expStr.split("+")
                     let num1= expARR[0]
@@ -100,22 +108,18 @@ document.querySelectorAll('button').forEach((button) => {
                     let num2= expARR[1]
                     subtract(num1,num2,runoffARR)
              }
-             if(expStr.includes("*")){
-                expARR= expStr.split("*")
-                let num1= expARR[0]
-                let num2= expARR[1]
-                multiply(num1,num2,runoffARR)
+                if(expStr.includes("*")){
+                    expARR= expStr.split("*")
+                    let num1= expARR[0]
+                    let num2= expARR[1]
+                    multiply(num1,num2,runoffARR)
             }
-            if(expStr.includes("÷")){
-                expARR= expStr.split("÷")
-                let num1= expARR[0]
-                let num2= expARR[1]
-                divide(num1,num2,runoffARR)
+                if(expStr.includes("÷")){
+                    expARR= expStr.split("÷")
+                    let num1= expARR[0]
+                    let num2= expARR[1]
+                    divide(num1,num2,runoffARR)
             }
         }
         
     }
- 
-      
-  }
-  
